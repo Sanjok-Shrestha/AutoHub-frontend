88e632b61b02be7e9ba2c9c71cd43a5e86bf3f34
sadiksha4 <sadiksha1357@gmail.com>
2026-05-08 17:51:46 +0545

    financial reports and PDF export Sadiksha

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import VendorPage from "./pages/VendorPage";
 import StaffPage from "./pages/StaffPage";
 import PurchaseInvoicePage from "./pages/PurchaseInvoicePage";
+import ReportsPage from "./pages/ReportsPage";
 
 function App() {
   const [page, setPage] = useState("vendor");
@@ -11,6 +12,7 @@ function App() {
       {page === "vendor" && <VendorPage setPage={setPage} />}
       {page === "staff" && <StaffPage setPage={setPage} />}
       {page === "invoice" && <PurchaseInvoicePage setPage={setPage} />}
+      {page === "reports" && <ReportsPage setPage={setPage} />}
     </>
   );
 }
