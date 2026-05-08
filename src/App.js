abbbd18367617945485eacdade27f086b14f1a39
sadiksha4 <sadiksha1357@gmail.com>
2026-05-08 12:44:30 +0545

    Connected Purchase Invoice frontend with backend Sadiksha

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import VendorPage from "./pages/VendorPage";
 import StaffPage from "./pages/StaffPage";
+import PurchaseInvoicePage from "./pages/PurchaseInvoicePage";
 
 function App() {
   const [page, setPage] = useState("vendor");
@@ -9,6 +10,7 @@ function App() {
     <>
       {page === "vendor" && <VendorPage setPage={setPage} />}
       {page === "staff" && <StaffPage setPage={setPage} />}
+      {page === "invoice" && <PurchaseInvoicePage setPage={setPage} />}
     </>
   );
 }
